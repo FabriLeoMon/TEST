a8819b98430212eb80d18ca9cc17b461dc5eef2e
montvlein <montvlein@gmail.com>
2023-11-02 17:00:40 -0300

    se agrego log

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const queryClient = new QueryClient();
 
+console.log(import.meta.env.VITE_GOOGLE_CLIENT_ID);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID} scopes={['email']}>
     <React.StrictMode>
